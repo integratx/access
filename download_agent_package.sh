68bfe455608a065feaeb4714bb0d1ee23333b0f2
Rick George <mail@rickgeorge.com>
2025-04-25 13:34:38 -0700

    Latest update

--- download_agent_package.sh
+++ download_agent_package.sh
@@ -1,24 +1,32 @@
 #!/bin/bash
 
-# Run this script with the provided token to download the integratx agent package
+# Run this script with the provided token and routing number to download the integratx agent package
+
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <token> <routing #>" >&2
+    exit 1
+fi
 
 # classic or fine-grained PAT with “read” on contents
-export GH_TOKEN=[pat]
+export GH_TOKEN="$1"
+export ROUTING_NUMBER="$2"
 
 OWNER="integratx"                # repo owner
 REPO="agent"
 TAG="v1.0.beta"
-NAME="itx-agent-package.tar.gz"
+PACKAGE="itx-agent-package"
+NAME="$PACKAGE.tar.gz"
 CHECK="checksum"
+CRED="$HOME/.git-credentials"
 
-# 1 – look up the asset id from the tagged release
+# 1 – look up the tar file id from the tagged release
 asset_id=$(curl -s \
   -H "Authorization: Bearer $GH_TOKEN" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   "https://api.github.com/repos/$OWNER/$REPO/releases/tags/$TAG" |
   jq -r --arg NAME "$NAME" '.assets[]|select(.name==$NAME)|.id')
 
-# 2 – stream the asset
+# 2 – stream the tar file
 curl -L \
   -H "Accept: application/octet-stream" \
   -H "Authorization: Bearer $GH_TOKEN" \
@@ -41,5 +49,13 @@ curl -L \
 -o "$CHECK" \
 "https://api.github.com/repos/$OWNER/$REPO/releases/assets/$checksum_id"
 
-# 5 - verify the checksum
-sha256sum -c <<< "$(cat checksum)"
+# 5 - verify the checksum - extract and store credentials if ok
+if sha256sum -c checksum; then
+    tar -xzf $NAME
+	echo "https://itxagent:$GH_TOKEN@github.com" >> $CRED
+	chmod 600 $CRED
+	echo "$ROUTING_NUMBER" > "$PACKAGE/routing.number"
+else
+    echo "Checksum verification failed."
+    exit 1
+fi
